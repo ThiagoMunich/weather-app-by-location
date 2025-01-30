4dce7c84b51013162e624ca0445f8539df3bebeb
Thiago Munich <thiago.munich@direcional.com.br>
2025-01-30 17:24:50 -0300

    refactor: remove unused Search route import

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import React from "react"
 import { createDrawerNavigator } from "@react-navigation/drawer"
 
 import Home from "./pages/Home"
-import Search from "./pages/Search"
 
 const Drawer = createDrawerNavigator()
 
